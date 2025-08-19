49d04953dbb7c19e5511d592cb9da916feeb1567
Julio Lab <juliolab90@hotmail.com>
2025-08-18 20:22:45 -0600

    Config: Update ESLint to include HTML plugin for formatting and linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import html from "@html-eslint/eslint-plugin";
 
 export default defineConfig([
   {
@@ -26,6 +27,34 @@ export default defineConfig([
     extends: ["markdown/recommended"]
   },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    // @html-eslint/eslint-plugin
+    ...html.configs["flat/recommended"],
+    files: ["**/*.html"],
+    plugins: { "@html-eslint": html }, // Explicit plugin configuration
+    rules: {
+      // ...html.configs["flat/recommended"].rules, // Must be defined. If not, all recommended rules will be lost
+      "@html-eslint/no-duplicate-id": "warn",
+      "@html-eslint/no-inline-styles": "warn",
+      "@html-eslint/no-obsolete-tags": "error", // default
+      "@html-eslint/require-button-type": "warn",
+      "@html-eslint/require-closing-tags": ["error", { selfClosing: "always" }],
+      "@html-eslint/use-baseline": ["warn", { available: "newly" }],
+      "@html-eslint/no-multiple-h1": "error", // default
+      "@html-eslint/lowercase": "error",
+      "@html-eslint/no-extra-spacing-attrs": [
+        "error",
+        {
+          enforceBeforeSelfClose: true,
+          disallowMissing: true,
+          disallowTabs: true,
+          disallowInAssignment: true
+        }
+      ],
+      "@html-eslint/no-multiple-empty-lines": ["error", { max: 1 }],
+      "@html-eslint/no-trailing-spaces": "error"
+    }
+  },
   eslintConfigPrettier,
   {
     rules: {
